5cabf4bf87004371feefb000aa2f3a59a0304b7c
repl.it user <replituser@example.com>
2020-10-05 16:01:42 +0000

    I have added code to display an ASCII robaot with dynamic eyes

---
+++ basics/input/ascii_art.py
@@ -0,0 +1,9 @@
+# Display a box
+X = input("Please enter a character for the eye")
+print("Beep's expression is now as follows:")
+print("##########")
+print("# ",X + X," #")
+print("#     o  #")
+print("##########")
+print("     ``   ")
+print("    //    ")
