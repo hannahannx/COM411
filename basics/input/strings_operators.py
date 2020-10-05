7251765932b5953a07f775f04b947cda7a4c297e
repl.it user <replituser@example.com>
2020-10-05 16:25:31 +0000

    I have added a code to shoe the use of string operators

---
+++ basics/input/strings_operators.py
@@ -0,0 +1,11 @@
+lives = int(input("Please enter the number of lives"))
+energy = int(input("Please enter the energy level."))
+shields = int(input("Please enter the shield level."))
+
+print("Health has been set.")
+print("Lives:")
+print ("♥" * lives)
+print("Energy:")
+print("♦" * energy)
+print("Shield:")
+print("♦" * shields)
