e9097cef0b905a422b4f8d725c8002b758a8c22f
repl.it user <replituser@example.com>
2020-10-05 16:14:15 +0000

    I have made a code to demostrate the use of data types

---
+++ basics/input/data_types.py
@@ -0,0 +1,8 @@
+name = str(input("What is your name human?"))
+age = int(input("How old are you (in years)?"))
+height = float(input("How tall are you (in meters"))
+weight = float(input("How much do you weigh in kilograms?"))
+
+bmi = weight / (height**2)
+
+print(name,"you are",age,"years old and your bmi is ",bmi)
