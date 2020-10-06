8d4fce763994db68271fe93101d5c0d74cbde4e6
repl.it user <replituser@example.com>
2020-10-06 10:51:51 +0000

    I have added comments

--- basics/decisions/simple_decision/if.py
+++ basics/decisions/simple_decision/if.py
@@ -1,4 +1,6 @@
+#Creating the varibles to ask what ype of book they have 
 book_type = str(input("What type of book is this?"))
+#Showing a message if the type ad
 if book_type == "adventure":
   print("I like adventure books!!")
 
