923a79a2113cecf9e8b1973ff1e8fd6380196081
repl.it user <replituser@example.com>
2020-10-06 10:21:21 +0000

    I have added a code to show the use of modulus operators

---
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,6 @@
+whole_number = int(input("Please enter a whole number"))
+
+if (whole_number % 2) != 0:
+  print("The number",whole_number,"is an odd number")
+else:
+  print(whole_number,"is an even number")
