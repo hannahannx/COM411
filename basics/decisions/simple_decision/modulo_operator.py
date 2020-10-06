3c95d7605a713a64d2f1fd70d8fbc7cd4067d536
repl.it user <replituser@example.com>
2020-10-06 10:58:30 +0000

    I have added comments to the code for modulus operators

--- basics/decisions/simple_decision/modulo_operator.py
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -1,5 +1,6 @@
+#Creating a variable for the whole number
 whole_number = int(input("Please enter a whole number"))
-
+#Creating the branches for the odd and even numbers 
 if (whole_number % 2) != 0:
   print("The number",whole_number,"is an odd number")
 else:
