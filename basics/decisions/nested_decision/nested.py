8ff523b0632cd85229e8eb985b30820701b3dad1
repl.it user <replituser@example.com>
2020-10-07 10:21:22 +0000

    I have added a code to show the use of a nested if statement

---
+++ basics/decisions/nested_decision/nested.py
@@ -0,0 +1,12 @@
+#Creating the variables
+cover_type = str(input("What type of cover does the book have?"))
+
+#deciding what type of cover it is and what response it will give out
+if cover_type == "soft":
+  shape = str(input("Is the book perfect-bound?"))
+  if shape == "yes":
+    print("Soft cover, perfect bound books are very popular!")
+  else:
+    print("Soft covers with coils or stitches are great for short books")
+else:
+  print("Books with hard covers can be more expensive!")
