2dc37699b4fb2b145b2e9c8289d0e42d1f173beb
repl.it user <replituser@example.com>
2020-10-07 10:08:35 +0000

    I have created a file to show modulus operators

---
+++ basics/decisions/simple_decision/counter.py
@@ -0,0 +1,24 @@
+#Creating the varibales
+num1 = int(input("Please enter the first whole number."))
+num2 = int(input("Please enter the second whole number."))
+num3 = int(input("Please enter the third whole number."))
+#Global varibales
+even = 0
+odd = 0
+#Creating the decisons for the even and odd numbers
+if (num1 % 2) == 0:
+  even = even + 1
+else:
+  odd = odd + 1
+
+if (num2 % 2) == 0:
+  even = even + 1
+else:
+  odd = odd + 1
+
+if (num3 % 2) == 0:
+  even = even + 1
+else:
+  odd = odd + 1
+
+print("There were",even,"even and",odd,"odd numbers")
