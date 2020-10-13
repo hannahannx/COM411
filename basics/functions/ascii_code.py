e48fc202a80c9f11380452679a8e156752ad6bf4
repl.it user <replituser@example.com>
2020-10-13 09:20:03 +0000

    Added code to show the use of built in functions len and ord

---
+++ basics/functions/ascii_code.py
@@ -0,0 +1,11 @@
+#Printing out the starting message an creating the message
+print("Program Started!")
+character = str(input("Please enter a standard character."))
+
+if (len(character) == 1):
+  print("The ASCII code for the letter {} is: {}".format(character,ord(character)))
+else:
+  print("You have not entered a character. ")
+
+  print("Program Ended")
+
