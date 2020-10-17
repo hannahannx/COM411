472452b5b7963851d7d0302320bbf2d540758265
repl.it user <replituser@example.com>
2020-10-17 21:39:04 +0000

    Added code to shoe counting down with a for loop

---
+++ basics/repetitions/for_loop/count_down.py
@@ -0,0 +1,7 @@
+#Creating user input
+steps =int(input("How many steps are we from the cave? "))
+#Creating loop printing out based on user input
+for step in range(steps,0,-1):
+  print("{} steps remaining".format(step))
+#Ending the loop and printing final message
+print("We have reached the cave!")
