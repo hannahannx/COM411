83e68d0f21d313b60660af780fffc1d97d946ebf
repl.it user <replituser@example.com>
2020-10-17 13:49:44 +0000

    Added code to show the use f while loop in performing calculations with user input

---
+++ basics/repetitions/while_loop/len.py
@@ -0,0 +1,8 @@
+#Creating user input
+phrase = str(input("Please enter a phrase: "))
+#creating a counter
+count = 0
+#Repeating the phrase
+while count != len(phrase):
+  print("Bop ", end = "")
+  count = count + 1
