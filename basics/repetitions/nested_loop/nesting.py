994a4955f42d56c648c8aeb15847f6793e915d0d
repl.it user <replituser@example.com>
2020-10-18 22:15:53 +0000

    Added code to show the use of nested loops and decisions

---
+++ basics/repetitions/nested_loop/nesting.py
@@ -0,0 +1,21 @@
+#Adking the user for user input
+sequence = str(input("Please enter a sequence: "))
+print()
+marker = str(input("Please enter the character for the marker: "))
+print()
+#Creating the markers
+marker1 = 0
+marker2 = 0
+#Finding the markers
+for find in range(0,len(sequence),1):
+  marks = sequence[find]
+  #Looking for the marker in the sequence
+  if marks == marker :
+    marker1 == find
+  else:
+    marker2 = find
+
+#calulating the difference in markers
+difference = marker2 - marker1
+#printing out the result
+print("The disatnce between the markers is {}".format (difference))
