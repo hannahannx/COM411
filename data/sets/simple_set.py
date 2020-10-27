8bc4d0e09a2d01d871a14a8d77c8a6ff65744258
repl.it user <replituser@example.com>
2020-10-27 10:02:48 +0000

    Added code to show a simple set

---
+++ data/sets/simple_set.py
@@ -0,0 +1,12 @@
+#Defining a funtion
+def observed ():
+  #initalising a set
+  observations = {"Flying Car", "Sky Scraper", "Laser" , "Dome"}
+  return observations
+
+#Defining a function to run the program 
+def run():
+  print(observed())
+
+#Calling the program
+run()
