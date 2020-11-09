a53e7a46eacd50ddd5e3fb2d2d080570797f4d43
repl.it user <replituser@example.com>
2020-11-09 00:02:38 +0000

    Added code to show how to manipulate and sort a set

--- data/sets/sorted_list.py
+++ data/sets/sorted_list.py
@@ -8,15 +8,36 @@ def observed ():
     observation = str(input("Please enter an observation: "))
     #adding the plaes after each iteration
     observations.append(observation)
+
   return observations
 
-def remove_observations(all_observations):
-  question = str(input("Would you like to remove observations?\n a) YES \n b) NO \n"))
-  if question.lower() == "a":
-    remove = True
-    while remove == True:
+def remove_observations(observations):
+  #preparing for a loop
+  remove = True
+  #Looping askng the question
+  while remove == True:
+    question = str(input("Would you like to remove observations?\n a) YES \n b) NO \n"))
+    if question.lower() == "a":
       observation_to_remove = str(input("What observation would you like to remove? "))
+      observations.remove(observation_to_remove)
+      print("Done!")
+      print("New list of observations: {}".format(observations))
+    else:
+      remove = False
 
+def run():
+  #Making sure that the values from the prefvious function can be accessed
+  all_observations = observed()
+  remove_observations(all_observations)
 
-  
-  
+  # populate set
+  observations_set = set()
+  for observation in all_observations:
+    data = (observation, all_observations.count(observation))
+    observations_set.add(data)
+
+  # display set
+  for data in sorted(observations_set):
+    print("{} observed {} time(s).".format(data[0],data[1]))
+
+run()
