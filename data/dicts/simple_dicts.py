51416a35df3139d4f7402fb1bd77ccc967bf730a
repl.it user <replituser@example.com>
2020-11-09 00:08:23 +0000

    Added code to show a simple dictionary

---
+++ data/dicts/simple_dicts.py
@@ -0,0 +1,16 @@
+#Creating a function storing no parameters
+def pattern ():
+  #CREATING A DICTIONARY WITH VALUES IN IT
+  sequences = {"Short Sequence":3, "Medium Sequence":2, "Long Sequence":1}
+  #RETURNING THE DICTONARY
+  return sequences
+
+#Creating a function to run the program
+def run():
+  print(pattern())
+
+#Running the program
+run()
+
+
+
