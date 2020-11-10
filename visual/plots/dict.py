3bfdd76e6e1a3831dca5b064716436760f973d95
repl.it user <replituser@example.com>
2020-11-10 12:41:10 +0000

    Added code to show line plotsusing a dictionary

---
+++ visual/plots/dict.py
@@ -0,0 +1,10 @@
+#importing module
+import matplotlib.pyplot as plt
+
+#Craeting a function
+def data():
+  paths = {}
+  lineType = input("What type of line  would you like? (dotted - d, dashed -d or solid - s)")
+  colourType = input("What type of colour  would you like? (red - r, green - r or blue - b)")
+  markerStyle = input("What marker style woud you like? (circle - c square -s or triangle - t)")
+  
