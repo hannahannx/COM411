750602fd0e7470251d7b2d5a99b0d50c04a680d1
repl.it user <replituser@example.com>
2020-11-20 20:44:00 +0000

    Added code for main file an subplots

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import basics.output.simple_message as simple_message
 import basics.output.multiline_message as multiline_message
+import basics.output.escape_characters as escape_characters
+import basics.output.ascii_art as ascii_art
 
 def run_block_a():
     print("Which program in 'Block A: Basics' do you wish to run?")
@@ -8,6 +10,10 @@ def run_block_a():
         simple_message.run()
     elif (response == "multiline_message"):
         multiline_message.run()
+    elif (response == "escape_characters"):
+        escape_characters.run()
+    elif (response == "ascii_art"):
+        ascii_art.run()
 
 
 def run():
@@ -16,6 +22,7 @@ def run():
     while(is_running):
         print("What would you like to do?")
         print("[a] Run 'Block A: Basics' programs")
+        print("[b] Run 'Block B: Data' programs ")
         print("[q] Quit")
         response = input()
 
