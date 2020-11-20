b10b8c131c0aee9ba8f08e68b069b14ee3554828
repl.it user <replituser@example.com>
2020-11-20 14:21:02 +0000

    Added code to shoe the use of subplots

--- visual/subplots/temps.py
+++ visual/subplots/temps.py
@@ -5,23 +5,27 @@ import matplotlib.pyplot as plt
 def read_data(fileName):
   #opens file
   with open(fileName) as textFile:
-    #reads each line and returns the data
+    temps = []
+    #reads each line and returns the data in temps
     for line in textFile:
-      temps = []
-      temps.append(line.strip())
+      temps.append(float(line.strip()))
     return temps
-    print(temps)
 
 #Creating a function
 def run():
   data = read_data("visual/subplots/temps.txt")
+  #making 1 row and 2 graphs 
   fig, axes = plt.subplots(1, 2)
-  x = range(7)
-  y = data  
 
+#making the x and y vaues
+  x = range(1,8)
+  y = data
+
+#prints out  line grapgh and bar chart with the temperartures
   axes[0].plot(x, y)
   axes[1].bar(x, y)
    
+  #making the layout fit on the page
   plt.tight_layout()
   plt.show()
 
